93231c5ae7d4a24d2b78d10546e7c0bffb12caab
Jer <le.mail.de.jeremy@gmail.com>
2019-07-04 20:54:42 -0400

    first commit

---
+++ Assets/Scripts/EncryptController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+
+public class EncryptController : MonoBehaviour
+{
+    public string ScriptUrl = "http://example.com/encrypt.php";
+
+    public InputField ClearTextInputField;
+    public InputField EncryptedTextInputField;
+    
+    public InputField ClearResponseInputField;
+    public Text PublicKeyText;
+
+    public void OnEncryptButtonClick()
+    {
+        if (PublicKeyText.text.Length < 100)
+        {
+            Debug.LogError("You must generate RSA key pair before");
+            return;
+        }
+        var csp = new RSACryptoServiceProvider(1024);
+        csp.FromXmlString(PublicKeyText.text);
+        var plainTextData = ClearTextInputField.text;
+        var bytesPlainTextData = Encoding.Unicode.GetBytes(plainTextData);
+        var bytesCypherText = csp.Encrypt(bytesPlainTextData, false);
+        var cypherText = Convert.ToBase64String(bytesCypherText);
+        EncryptedTextInputField.text = cypherText;
+    }
+
+    public void OnKeygenButtonClick()
+    {
+        StartCoroutine(GenerateRsaKeyPair());
+    }
+
+    public void OnDecryptButtonClick()
+    {
+        if (PublicKeyText.text.Length < 100)
+        {
+            Debug.LogError("You must generate RSA key pair before");
+            return;
+        }
+        StartCoroutine(SendEncryptedText(EncryptedTextInputField.text));
+    }
+
+    IEnumerator GenerateRsaKeyPair()
+    {
+        var form = new WWWForm();
+        form.AddField("keygen", 1);
+
+        using (var www = UnityWebRequest.Post(ScriptUrl, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                //var sb = new StringBuilder();
+                //foreach (var dict in www.GetResponseHeaders())
+                //{
+                //    sb.Append(dict.Key).Append(": \t[").Append(dict.Value).Append("]\n");
+                //}
+
+                //// Print Headers
+                //Debug.Log(sb.ToString());
+
+                // Print Body
+                Debug.Log(www.downloadHandler.text);
+                PublicKeyText.text = www.downloadHandler.text;
+            }
+        }
+    }
+
+    IEnumerator SendEncryptedText(string text)
+    {
+        var form = new WWWForm();
+        form.AddField("decrypt", text);
+
+        using (var www = UnityWebRequest.Post("https://dev.payment-contest.fr/php/testencrypt.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+                ClearResponseInputField.text = www.downloadHandler.text;
+            }
+        }
+    }
+}
---
+++ www/encrypt.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * test with curl:
+ *
+ *  // Generate keypair : return XML public key
+ *  curl -c cookies.txt -d "keygen=1" http://exemple.com//encrypt.php
+ *
+ *  // Test encrypt/decrypt : return encrypted and decrypted `my text to encode`
+ *  curl -b cookies.txt -d "test=my text to encode" http://exemple.com//encrypt.php
+ *
+ *  // Test encrypt : return encrypted
+ *  curl -b cookie.txt -d "encrypt=my text to encode" http://exemple.com//encrypt.php > encrypted.txt; cat encrypted.txt
+ *
+ *  // Test decrypt : return decrypted `my text to encode`
+ *  curl -b cookie.txt -d "decrypt=`cat encrypted.txt`" http://exemple.com//encrypt.php
+ */
+
+session_start();
+
+include 'vendor/autoload.php';
+use phpseclib\Crypt\RSA;
+
+function generateKeyPair(){
+    if (!isset($_SESSION['publickey'])){
+        $rsa = new RSA();
+        $rsa->setPrivateKeyFormat(RSA::PRIVATE_FORMAT_XML);
+        $rsa->setPublicKeyFormat(RSA::PUBLIC_FORMAT_XML);
+        $keys = $rsa->createKey();
+        $_SESSION['privatekey'] = $keys['privatekey'];
+        $_SESSION['publickey'] = $keys['publickey'];
+    }
+    return $_SESSION['publickey'];
+}
+
+function encrypt($cleartext){
+    $rsa = new RSA();
+    $rsa->setEncryptionMode(RSA::ENCRYPTION_PKCS1);
+    $rsa->setPublicKeyFormat(RSA::PUBLIC_FORMAT_XML);
+    $rsa->loadKey($_SESSION['publickey']);
+    $bytesCipherText = $rsa->encrypt($cleartext);
+    return rawurlencode(base64_encode($bytesCipherText));
+}
+
+function decrypt($encrypted){
+    $rsa = new RSA();
+    $rsa->setEncryptionMode(RSA::ENCRYPTION_PKCS1);
+    $rsa->setPrivateKeyFormat(RSA::PRIVATE_FORMAT_XML);
+    $rsa->loadKey($_SESSION['privatekey']);
+    $bytesCipherText = base64_decode(rawurldecode($encrypted));
+    return $rsa->decrypt($bytesCipherText);
+}
+
+if (isset($_POST['session'])) {
+    echo $_COOKIE['PHPSESSID'];
+}
+
+if (isset($_POST['keygen'])) {
+    echo generateKeyPair();
+    exit();
+}
+
+if (isset($_POST['encrypt'])) {
+    echo encrypt($_POST['encrypt']);
+    exit();
+}
+
+if (isset($_POST['decrypt'])) {
+    echo decrypt($_POST['decrypt']);
+    exit();
+}
+
+if (isset($_POST['test'])) {
+    generateKeyPair();
+    $ciphertext = encrypt($_POST['test']);
+    echo "encrypted: $ciphertext\n\n";
+    $clearText = decrypt($ciphertext);
+    echo "decrypted: $clearText\n";
+    exit();
+}
